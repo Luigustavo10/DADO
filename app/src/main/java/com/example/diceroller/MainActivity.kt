f046845f0c64b7abe40ac1c985ad7cc145dec503
luis <luis>
2022-09-27 22:45:46 -0300

    TOAST

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+
+
+    }
+    class Dice(val numSides: Int) {
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dados
+            2 -> R.drawable.dado2
+            3 -> R.drawable.dado3
+            4 -> R.drawable.dado4
+            5 -> R.drawable.dado5
+            else -> R.drawable.dado6
+        }
+            diceImage.contentDescription = diceRoll.toString()
+        }
+    }
+
+
+
